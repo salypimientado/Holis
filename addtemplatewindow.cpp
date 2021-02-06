994f718b562a5983500e2c6823328cd47eb606ff
salypimientado <juanpablosalazar63@hotmail.com>
2021-02-06 14:10:58 -0700

    template window done (crashes on less than three parameters for some reason)

--- addtemplatewindow.cpp
+++ addtemplatewindow.cpp
@@ -5,49 +5,78 @@
 #include <QPushButton>
 #include <QLineEdit>
 #include <QComboBox>
+#include <QScreen>
+#include <QGuiApplication>
 
 AddTemplateWindow::AddTemplateWindow(QWidget *parent) : QWidget(parent)
 {
+    QScreen *screen = QGuiApplication::primaryScreen();
+    QRect  screenGeometry = screen->geometry();
+    int height = screenGeometry.height();
+    int width = screenGeometry.width();
+    resize(width/3,height/2);
+    nameLineEditList.reserve(30);
+    valueLineEditList.reserve(30);
+    typeComboboxList.reserve(30);
 
     QGridLayout* windowLayout = new QGridLayout(this);
-    QWidget* parameterWidget = new QWidget();
-    QVBoxLayout* parameterLayout = new QVBoxLayout();
+    QHBoxLayout* parameterLayout = new QHBoxLayout();
 
-    parameterWidget->setLayout(parameterLayout);
+    nameLineEditLayout = new QVBoxLayout;
+    valueLineEditLayout = new QVBoxLayout;
+    typeComboboxLayout = new QVBoxLayout;
 
     windowLayout->setColumnStretch(0,1);
-    //windowLayout->setRowStretch(0,1);
-    //windowLayout->setColumnStretch(1,1);
-    //windowLayout->setRowStretch(1,1);
-    //windowLayout->setColumnStretch(2,1);
+    windowLayout->setRowStretch(0,1);
+    windowLayout->setColumnStretch(1,3);
+    windowLayout->setRowStretch(1,1);
+    windowLayout->setColumnStretch(2,1);
     windowLayout->setRowStretch(2,1);
 
-    auto titleLabel = new QLabel(tr("Add task"));
+    auto titleLabel = new QLabel(tr("Add task template"));
     auto titleFont = titleLabel->font();
     titleFont.setBold(1);
     titleFont.setPointSize(20);
     titleLabel->setFont(titleFont);
     windowLayout->addWidget(titleLabel,0,1,Qt::AlignHCenter);
-    windowLayout->addWidget(parameterWidget,1,1);
+    windowLayout->addLayout(parameterLayout,1,1);
+    parameterLayout->addLayout(nameLineEditLayout,2);
+    parameterLayout->addLayout(valueLineEditLayout,2);
+    parameterLayout->addLayout(typeComboboxLayout,1);
 
-    QHBoxLayout* labels = new QHBoxLayout();
-    labels->setAlignment(Qt::AlignHCenter);
-    labels->addWidget(new QLabel("Name"),2);
-    labels->addWidget(new QLabel("Value"),2);
-    labels->addWidget(new QLabel("Type"),1);
-    parameterLayout->addLayout(labels);
+    QLabel* nameLabel = new QLabel("Name");
+    nameLabel->setAlignment(Qt::AlignBottom);
+    nameLineEditLayout->addWidget(nameLabel,1);
 
-    for(int i = 0; i<3;i++){
-    QWidget* fieldWidget = new QWidget();
-    QHBoxLayout* parameterFields = new QHBoxLayout(fieldWidget);
-        QLineEdit* edit = new QLineEdit;
-        QLineEdit* nameEdit = new QLineEdit;
+    QLabel* valueLabel = new QLabel("Value");
+    valueLabel ->setAlignment(Qt::AlignBottom);
+    valueLineEditLayout->addWidget(valueLabel,1);
+
+    QLabel* typeLabel = new QLabel("Type");
+    typeLabel->setAlignment(Qt::AlignBottom);
+    typeComboboxLayout->addWidget(typeLabel,1);
+
+
+    for(int i = 0; i<1;i++){
+        addParameter();
+    }
+    nameLineEditList[0]->setText("prueba wtf");
+    valueLineEditList[2]->setText("prueba wwtfasdftf");
+    nameLineEditList[1]->setText("prueba hwaksalj");
+
+}
+
+void AddTemplateWindow :: addParameter(){
+        QLineEdit* edit = new QLineEdit();
+        valueLineEditList.append(edit);
+        QLineEdit* nameEdit = new QLineEdit();
+        nameLineEditList.append(nameEdit);
         QComboBox* box = new QComboBox();
+        typeComboboxList.append(box);
         box->addItem("Float");
         box->addItem("Bool");
-        parameterFields->addWidget(nameEdit);
-        parameterFields->addWidget(edit);
-        parameterFields->addWidget(box);
-        parameterLayout->addWidget(fieldWidget);
-    }
+        nameLineEditLayout->addWidget(nameEdit);
+        valueLineEditLayout->addWidget(edit);
+        typeComboboxLayout->addWidget(box);
+
 }
--- addtemplatewindow.h
+++ addtemplatewindow.h
@@ -2,13 +2,25 @@
 #define ADDTEMPLATEWINDOW_H
 
 #include <QWidget>
+#include <QLineEdit>
+#include <QComboBox>
+#include <QVBoxLayout>
 
 class AddTemplateWindow : public QWidget
 {
     Q_OBJECT
 public:
     explicit AddTemplateWindow(QWidget *parent = nullptr);
+    void addParameter();
 
+private:
+    QVector<QLineEdit*> nameLineEditList;
+    QVector<QLineEdit*> valueLineEditList;
+    QVector<QComboBox*> typeComboboxList;
+
+    QVBoxLayout* nameLineEditLayout;
+    QVBoxLayout* valueLineEditLayout;
+    QVBoxLayout* typeComboboxLayout;
 signals:
 
 };
